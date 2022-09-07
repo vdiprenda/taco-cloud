0cf00a00adbd7e2ffd7cad4124f710f7ccacedbc
Vincent DiPrenda <vdiprenda@gmail.com>
2022-09-07 11:08:11 -0400

    Initial commit

---
+++ src/main/java/tacos/HomeController.java
@@ -0,0 +1,15 @@
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller            // <1>
+public class HomeController {
+
+  @GetMapping("/")     // <2>
+  public String home() {
+	  System.out.println("in the mapping controller");
+    return "home";     // <3>
+  }
+
+}
